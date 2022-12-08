3e377dcf51bbc4a3dae52d34bfd02530fa817339
JoyWu <eq0417@gmail.com>
2022-12-08 14:05:57 +0800

    second commit

---
+++ triangle.py
@@ -0,0 +1,13 @@
+for i in range(1,4):
+
+    for k in range(i,4):
+        print(" ",end=" ")
+    for j in range(i):
+        if i>=1 and i<= 2:
+            if j ==0 or j==i-1:
+                print("* ",end=" ")
+            else: 
+                print("  ",end=" ")
+        else:
+            print("* ",end=" ")
+    print()Cathay
